60bd097f7a881b5f838cc39fc20104e77e422af0
parag <adhauparag64@gmail.com>
2020-07-24 14:38:35 +0530

    use case 7 added

---
+++ uc7.sh
@@ -0,0 +1,33 @@
+#!bin/bash -x
+
+isfulltime=2
+ispartime=1
+EMP_RATE_PER_HRS=20
+TOTAL_EMPWAGE=0
+TOTAL_EMPHRS=0
+NUM_OF_WORKING_DAYS=20
+MAX_HRS_IN_MONTH=100
+while [ $TOTAL_EMPHRS -le $MAX_HRS_IN_MONTH ]
+do
+ empcheck=$((RANDOM%2))
+
+ case $empcheck in 
+      $isfulltime)
+         emphrs=8
+	;;
+      $ispartime)
+	 emphrs=4
+	;;
+ *)
+	emphrs=0
+        ;;
+esac
+        empwage=$(( $emphrs * $EMP_RATE_PER_HRS ))
+        echo "Daily Wage : $empwage"
+        TOTAL_EMPHRS=$(($TOTAL_EMPHRS+$emphrs ))
+        TOTAL_EMPWAGE=$(( $TOTAL_EMPWAGE+$empwage ))
+
+done
+
+         echo "Total Employee Wage=$TOTAL_EMPWAGE"
+
